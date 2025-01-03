7ebd368ecb81b94613189c6e80ada10470b44a93
locedu <locutus.edu@outlook.com>
2025-01-02 20:57:58 -0500

    add icons on Book list view.

--- src/components/BookListItem/BookListItem.jsx
+++ src/components/BookListItem/BookListItem.jsx
@@ -3,21 +3,38 @@ import ReactDOM from "react-dom/client";
 import { Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export default function BookListItem({ num, id, title, author, available, book }) {
+export default function BookListItem({
+  num,
+  id,
+  title,
+  author,
+  available,
+  book,
+}) {
   return (
     // <p>{num+1}. {title}</p>
     <tr>
       <td>{num + 1}</td>
       <td>
-        <Link to={`/books/${id}`}>
-          {title}
-        </Link>
+        <Link to={`/books/${id}`}>{title}</Link>
       </td>
 
       <td>{author}</td>
       {/* <td>{available.toString()}</td> */}
-      {available && <td className="book-item-available">Available</td>}
-      {!available && <td className="book-item-checked-out">Checked Out</td>}
+      {available && (
+        <td className="book-item book-item-available">
+          <div>
+          {/* <i className="material-icons">radio_button_checked</i> */}
+          <i className="material-icons">star</i>
+          </div>
+          <div>Available</div>
+        </td>
+      )}
+      {!available && <td className="book-item book-item-checked-out">
+        <div>
+          <i className="material-icons">radio_button_unchecked</i>
+          </div>
+          <div>Checked Out</div></td>}
     </tr>
   );
 }
