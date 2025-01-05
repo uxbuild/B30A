d01f497e5e6c2e21b37de267c37f4cc058fd1497
locedu <locutus.edu@outlook.com>
2025-01-05 17:01:16 -0500

    CLEAN UP complete.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import App from "./App.jsx";
-// import './index.css'
-{
-  /* The following line can be included in your src/index.js or App.js file */
-}
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
