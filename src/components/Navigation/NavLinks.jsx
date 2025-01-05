039eb40a608de68fe687a2af7865d63e0f23f278
locedu <locutus.edu@outlook.com>
2025-01-05 16:04:18 -0500

    CLEAN UP navigation, navigation login, links, search field.

--- src/components/Navigation/NavLinks.jsx
+++ src/components/Navigation/NavLinks.jsx
@@ -1,11 +1,6 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import { useLocation } from "react-router-dom";
+
 import { NavLink } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, } from "react-redux";
 import { getLogin } from "../../store/confirmLoginSlice";
 
 export default function NavLinks() {
--- src/components/Navigation/NavSearchField.jsx
+++ src/components/Navigation/NavSearchField.jsx
@@ -1,23 +1,13 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
 import { useSelector, useDispatch } from "react-redux";
 import { setSearchKey, getSearchKey } from "../../store/searchKeySlice";
-import { Divider } from "@mui/material";
 
 export default function NavSearchField() {
   const dispatch = useDispatch();
   const searchKey = useSelector(getSearchKey);
-  // const [search, setSearch] = useState("");
-
-  //clear search to reset book list view.
-  // dispatch(setSearchKey(""));
 
   function onSearchChange(e) {
     e.preventDefault();
-    console.log("search", e.target.value);
     dispatch(setSearchKey(e.target.value));
   }
 
